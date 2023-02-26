971c2894307fc8988aabed915870b2d09052eaa6
Shahrzad Saremi <103140910+Shazisaremi@users.noreply.github.com>
2023-02-26 12:46:31 +0330

    Update shazi_module_1.py

--- Module 1/Learning Activity 4/shazi_module_1.py
+++ Module 1/Learning Activity 4/shazi_module_1.py
@@ -1,1 +1,16 @@
+name = "Shazi"
+print(f"Welcome to Python! My name is {name}")
 
+x = 5
+y = 1
+
+a = x + y 
+b = x - y 
+c = x * y 
+d = x / y
+
+name = input("what is your name ?")
+address = input("Where are you living ?")
+phone = input("What is your phone number ?")
+
+print("a \n b \n c \n d \n name \n address \n phone")
