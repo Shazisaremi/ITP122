805333b6d3dceb50d149e2a8da86da256158e46f
Shahrzad Saremi <103140910+Shazisaremi@users.noreply.github.com>
2023-03-01 15:12:11 +0330

    Update Exercise.py

--- Module 2/Learning_Activity_3/Exercise.py
+++ Module 2/Learning_Activity_3/Exercise.py
@@ -1,1 +1,27 @@
 
+print("3 > 4 ?", 3 > 4)
+
+print("3 == 4 ?", 3 == 4)
+
+print("15 != 3 ?", 15 != 3)
+
+print("6 + 4 =", 6 + 4)
+
+print("((27 * 2) + 46) ** 0.5 =", ((27 * 2) + 46) ** 0.5)
+
+print("Find the modulus of 35 / 6", 35/6)
+
+x = 200
+
+print("x > 4 or x < 300", x > 4 or x < 300)
+
+print("3 != 4 and 3 < 4", 3 != 4 and 3 < 4)
+
+x = 200
+
+print("not(x > 4 or x < 300)", not(x > 4 or x < 300))
+
+if x > 0:
+  x == 10
+  print("x is a positive number")
+# Assign 10 to x. If x is bigger than 0, print "x is a positive number".
