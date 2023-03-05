64f3cc4f92f6eb44619510bf29bf93573c6b6793
Shahrzad Saremi <103140910+Shazisaremi@users.noreply.github.com>
2023-03-05 14:56:59 +0330

    Update Exercise 2.py

--- Module 3/Exercise 2.py
+++ Module 3/Exercise 2.py
@@ -1,10 +1,10 @@
-age = int(input("Enter your age: \n"))
-income = int(input("Enter your income: \n"))
-if age > 18 and income < 20000:
+age = int(input("Enter your age: \n")) # Get the user's age and convert it to integer then store it to age variable.
+income = int(input("Enter your income: \n")) # Get the user's income and convert it to intetger then store it to age variable.
+if age > 18 and income < 20000: # Check the condition 
     print('You are eligible for financial help scheme.')
-elif age > 18 and income >20000:
+elif age > 18 and income >20000: # Check the condition 
     print('You are not eligible for financial help scheme.')
-elif age <18 and income <20000:
+elif age <18 and income <20000: # Check the condition
     print('You are not eligible for financial help scheme.')
-else:
+else: # Else none of conditions above were true, then do: 
     print('You are not eligible for financial help scheme.')
