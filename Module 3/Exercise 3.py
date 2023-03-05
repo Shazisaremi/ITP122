34f82fbdd1d7c9945a97898b68b965d7288aa04d
Shahrzad Saremi <103140910+Shazisaremi@users.noreply.github.com>
2023-03-05 14:58:37 +0330

    Update Exercise 3.py

--- Module 3/Exercise 3.py
+++ Module 3/Exercise 3.py
@@ -1,13 +1,13 @@
-marks = int(input("Enter your marks: \n"))
-if marks <50:
+marks = int(input("Enter your marks: \n")) # Get the user's marks and convert it to integer, then store it in marks variable
+if marks <50: # Check the condition 
     print("Grade = Fail (F)")
-elif marks>=50 and marks <60:
+elif marks>=50 and marks <60: # Check the condition 
     print("Grade = Pass (P)")
-elif marks>=65 and marks <75:
+elif marks>=65 and marks <75: # Check the condition
     print("Grade = Credit (CR)")
-elif marks>=75 and marks <90:
+elif marks>=75 and marks <90: # Check the condition 
     print("Grade = Distinction (D)")
-elif marks>=90:
+elif marks>=90: # Check the condition
     print("Grade = High Distinction (HD)")
-else:
+else: # Else none of above condition were true, : 
     print("Error! Enter a valid value.")
